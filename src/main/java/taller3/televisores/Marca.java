44e20d60aa4650bf59cc0e7d4c2f7884fec5a10c
Mifonsecaa <92172868+Mifonsecaa@users.noreply.github.com>
2022-03-26 21:50:47 -0500

    Create Marca.java

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,18 @@
+package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+	public Marca(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	void setNombre(String nombre) {
+		this.nombre = nombre;
+		
+	}
+	
+	String getNombre() {
+		return nombre;
+	}
+
+}
